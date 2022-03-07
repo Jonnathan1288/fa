46bbe227578790dbe719d06d8b06f70dff607a90
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2022-03-07 17:27:55 -0500

    Creación de proyecto.
    Se creo el proyecto satisfactoriamente.

---
+++ APPPP/src/apppp/APPPP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apppp;
+
+/**
+ *
+ * @author DELL
+ */
+public class APPPP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola como esatas esperoq ue estes bien");
+    }
+    
+}
